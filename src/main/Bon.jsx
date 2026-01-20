053fc3e1af3112033ce63cbc0eb6e4c8bcad9c33
micruz1972 <michaelvictorenuemezie@gmail.com>
2026-01-20 05:04:10 -0800

    updated the Bon component

---
+++ src/main/Bon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bon = () => {
+  return (
+    <div>
+      <p>start</p>
+    </div>
+  )
+}
+
+export default Bon
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Forces Vite to check for file changes
+      interval: 100,    // Checks every 100ms
+    },
+    host: true,         // Ensures the server is accessible
+    strictPort: true,
+  },
 })
